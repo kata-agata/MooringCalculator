2e7fb134cf1f38ff40bd6b8a2d421a37fbbf984c
kata-agata <34109598+kata-agata@users.noreply.github.com>
2018-04-25 22:49:33 +0200

    Created JavaFx, maven project with Spring Boot

---
+++ src/main/java/com/trend/mooringcalculator/MainApp.java
@@ -0,0 +1,51 @@
+package com.trend.mooringcalculator;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MainApp extends Application {
+
+        private ConfigurableApplicationContext context;
+    private Parent rootNode;
+    
+        @Override
+    public void init() throws Exception {
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(MainApp.class);
+        context = builder.run(getParameters().getRaw().toArray(new String[0]));
+ 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        loader.setControllerFactory(context::getBean);
+        rootNode = loader.load();
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        Scene scene = new Scene(rootNode);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
